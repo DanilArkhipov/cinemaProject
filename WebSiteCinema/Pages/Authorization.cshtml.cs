a77d0c50cd5b54342c68e783fc41292af33812b8
DanilArkhipov <danilarkhipov00@gmail.com>
2020-11-08 22:05:52 +0300

    Authorization form work correctly and add data to DB

--- WebSiteCinema/Pages/Authorization.cshtml.cs
+++ WebSiteCinema/Pages/Authorization.cshtml.cs
@@ -1,4 +1,7 @@
+using System;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository;
+using WebSiteCinema.Models;
 
 namespace WebSiteCinema.Pages
 {
@@ -7,6 +10,13 @@ namespace WebSiteCinema.Pages
         public void OnGet()
         {
             
+        }
+
+        async public void OnPostAsync(string login,string password,string email,string phone)
+        {
+            var user = new Users(0,login,password,0,0,email,phone);
+            SqlServerRepository<Users> rep = new SqlServerRepository<Users>();
+            await rep.InsertAsync(user);
         }
     }
 }
