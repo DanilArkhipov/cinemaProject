b1e34f6e07cdff39dcf22a0a4a078d360d5e2ccf
DanilArkhipov <danilarkhipov00@gmail.com>
2020-11-08 22:32:42 +0300

    Added OnPost handler, which correctly find record in DB by form's data

--- WebSiteCinema/Pages/Authorization.cshtml.cs
+++ WebSiteCinema/Pages/Authorization.cshtml.cs
@@ -1,12 +1,19 @@
+using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository;
+using WebSiteCinema.Models;
 
 namespace WebSiteCinema.Pages
 {
     public class Authorization : PageModel
     {
-        public void OnGet()
+        public async void OnPost(string login,string password)
         {
-            
+            var rep = new SqlServerRepository<Users>();
+            var users = await rep.GetAllAsync();
+            var res = users.Where(x => x.login == login && x.password == password).FirstOrDefault();
+            if (res!=null) Console.WriteLine(res.id);
         }
     }
 }
