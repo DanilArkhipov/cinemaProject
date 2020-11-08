78f0747c00a25e73add5b9b02ef27babe41fed50
DanilArkhipov <danilarkhipov00@gmail.com>
2020-11-08 21:52:40 +0300

    Added simple authorization form

---
+++ WebSiteCinema/Pages/Authorization.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebSiteCinema.Pages
+{
+    public class Authorization : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
--- WebSiteCinema/Pages/Index.cshtml.cs
+++ WebSiteCinema/Pages/Index.cshtml.cs
@@ -21,38 +21,7 @@ namespace WebSiteCinema.Pages
 
         async public void OnGet()
         {
-            Users usr1 = new Users
-            (
-                default(long),
-                "dddd",
-                "pppp",
-                (byte)1,
-                (byte)1,
-                "ffff",
-                "112223123"
-            ); 
-            Users usr2 = new Users
-            (
-                default(long),
-                "gspd",
-                "rw",
-                (byte)0,
-                (byte)0,
-                "gspd",
-                "88005553535"
-            );
-            SqlServerRepository<Users> rep = new SqlServerRepository<Users>();
-            await rep.InsertAsync(usr1);
-            await rep.InsertAsync(new []{usr1,usr2});
-            var res1 = await rep.GetByIdAsync(1);
-            Console.WriteLine(res1.FirstOrDefault().id);
-            var res2 = await rep.GetAllAsync();
-            foreach (var v in res2)
-            {
-                Console.WriteLine(v.id);
-            }
-            await rep.DeleteAsync(71);
-            await rep.UpdateAsync(72, usr1);
+            
         }
     }
 }
